211dbf8d0d3084303b8102ad1259b9eee9f7aeaf
Denise <97702569+Enghellcraft@users.noreply.github.com>
2023-09-04 19:35:19 -0300

    2PW created

---
+++ py_mut_excl.py
@@ -0,0 +1,115 @@
+#
+# TRABAJO PRACTICO DE SEMINARIO
+#
+#ALUMNOS:                                                                      
+#       • Bardales, Wilfredo                                                  
+#       • Martin, Denise                                                     
+#       • Paleari, Carolina                                                  
+
+# imports
+import random
+import threading
+import time
+
+# Global Variables
+
+
+# Functions
+
+
+#  null) Task + Pres
+print("                                                                                  ")
+print("**********************************************************************************")
+print("*               SEMINARIO DE PROGRAMACION - 2023 - TP EXCLUSION MUTUA            *")
+print("**********************************************************************************")
+print("    ALUMNOS:                                                                      ")
+print("            • Bardales, Wilfredo                                                  ")
+print("            • Martin, Denise                                                      ")
+print("            • Paleari, Carolina                                                   ")
+print("                                                                                  ")
+print("**********************************************************************************")
+print("*                                   OBJETIVO                                     *")
+print("**********************************************************************************")
+print("  Entendimiento de funcionamiento y errores producidos en casos de Exclusión Mutua.")
+print("                                                                                  ")
+print("**********************************************************************************")
+print("*                                   CONSIGNAS                                    *")
+print("**********************************************************************************")
+print("                                                                                  ")
+print("  EJERCICIO 1:                                                                    ")
+print("  Escribir en pseudocódigo el primer intento de solución de exclusión mutua       ")
+print("  (que usa turnos) para N procesos.                                               ")
+print("                                                                                  ")
+print("  EJERCICIO 2:                                                                    ")
+print("  Para Bakery de 2 procesos p y q, describir un escenario en que las variables    ")
+print("  np y nq crecen ilimitadamente.                                                  ")
+print("                                                                                  ")
+print("  EJERCICIO 3:                                                                    ")
+print("  Escribir una versión de Bakery para 3 procesos, lo más simple que se pueda.     ")
+print("                                                                                  ")
+
+#  I) Theory
+print("                                                                                  ")
+print("**********************************************************************************")
+print("*                                      TEORIA                                    *")
+print("**********************************************************************************")
+print("                                                                                  ")
+print("                         ********* CONCURRENCIA *********                         ")
+print("                                                                                  ")
+print(" Es la capacidad de un sistema de procesar mas de un hilo de ejecución (thread o  ")
+print(" proceso) al mismo tiempo.                                                        ")
+print("                                                                                  ")
+print("                            ********* THREAD *********                            ")
+print("                                                                                  ")
+print(" Es un proceso o unidad básica del sistema operativo que contiene toda la         ")
+print(" información para ser ejecutado, es decir, tiempo del procesador asignado.        ")
+print("                                                                                  ")
+print("                  ********* EJECUCIÓN NO DETERMINISTA *********                   ")
+print("                                                                                  ")
+print(" Es la ejecución de varios procesos en sus infinitas posibilidades de combinación ")
+print(" de orden, es decir, que se desconoce el orden de ejecución de dichos procesos.   ")
+print("                                                                                  ")
+print("                      ********* SECCIÓN CRÍTICA *********                         ")
+print("                                                                                  ")
+print(" Porción de código que debe ser ejecutada única y atómicamente.                   ")
+print("                                                                                   ")
+print("                           ********* SEMÁFORO *********                           ")
+print("                                                                                  ")
+print(" Variable o tipo de dato abstracto utilizado para el acceso a un recurso común    ")
+print(" requerido por múltiples threads, o para evitar problema de acceso a sección      ")
+print(" crítica en sistemas concurrentes.                                                ")
+print("                                                                                  ")
+print("                      ********* EXCLUSIÓN MUTUA *********                         ")
+print("                                                                                  ")
+print(" Evitar que varios hilos accedan simultáneamente a sección crítica, esencialmente")
+print(" para evitar condiciones de carrera y garantizar la integridad de los datos       ")
+print(" compartidos.                                                                     ")
+print("                                                                                  ")
+
+# II) Development
+print("                                                                                  ")
+print("**********************************************************************************")
+print("*                        SOLUCION A EXCLUSION MUTUA                              *")
+print("**********************************************************************************")
+print("                                                                                  ")
+
+print("                                                                                  ")
+print("**********************************************************************************")
+print("*                        BAKERY ALGORITHM: 2 PROCESOS                            *")
+print("**********************************************************************************")
+print("                                                                                  ")
+
+print("                                                                                  ")
+print("**********************************************************************************")
+print("*                        BAKERY ALGORITHM: N PROCESOS                            *")
+print("**********************************************************************************")
+print("                                                                                  ")
+   
+
+    
+# III)  Conclusions
+print("**********************************************************************************")
+print("*                                CONCLUSIONES                                    *")
+print("**********************************************************************************")
+print("  BLAH")
+
